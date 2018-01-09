0d4761c95df1f0a57752c8211ee6184e0f611aa6
peterjcaulfield <peterjcaulfield@gmail.com>
2018-01-09 22:46:48 +0000

    closes #1

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 brew install neovim
 # get pip3
 brew install python3
-# get neovim pythin interface
+# get neovim python interface
 pip3 install neovim
+# get fuzzy search dep ag
+brew install the_silver_searcher
